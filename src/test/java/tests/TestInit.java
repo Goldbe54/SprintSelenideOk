d3ae727b6659a387ef61f540da8b7558aa5ac7eb
Sofia Shevchuk <sofiashevtcuk2.04@gmail.com>
2023-12-14 15:49:21 +0200

    added mvn prifile

--- src/test/java/tests/TestInit.java
+++ src/test/java/tests/TestInit.java
@@ -3,18 +3,30 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
+import lombok.SneakyThrows;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
+import utils.SuiteConfiguration;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestInit {
 
     public static final Logger logger = LogManager.getLogger(TestInit.class);
+    private String environment;
+    private  Boolean headless;
+    private  String browser;
+
+    @BeforeSuite(alwaysRun = true)
+    @SneakyThrows
+    public void initTestSuite() {
+        SuiteConfiguration config = new SuiteConfiguration();
+        environment = config.getProperty("site.url");
+        headless = Boolean.parseBoolean(config.getProperty("headless"));
+        browser = config.getProperty("browser.mode");
+    }
+
 
     @Parameters({"browser", "headless"})
     @BeforeMethod
